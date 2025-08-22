3a7727d4cf67a236769672205b8ed156bb3bb631
Aslı Nur Aksakal <92970740+asliak@users.noreply.github.com>
2025-08-22 21:58:00 +0300

    Update and rename py_beg_tut_3.py to py_beg_tut_3- Operators and User Input.py

---
+++ py_beg_tut_3- Operators and User Input.py
@@ -0,0 +1,40 @@
+print(8 + 2)
+print(8 - 2)
+print(8 * 2)
+print(8 / 2)
+
+print(12 % 3)
+print(2 ** 5)
+
+#floor division
+print(10 // 3)
+print(-10 // 3)
+
+x = 10
+x = x + 10 # x += 10, can be done with other operators too
+print(x)
+
+x = 10
+y = 20
+
+print(x == y)
+print(x < y)
+print(x <= y)
+print(x != y)
+
+x = "Hello"
+y = "Hello"
+
+print(x == y)
+
+# we also have and, or, not operators
+print(not True)
+print(not 10 < 20)
+print(not 1 > 20)
+
+x = input("Enter number one: ") # by default input is string, need to typecast to use ıt
+y = input("Enter number two: ")
+
+x = int(x)
+y = int(y)
+print(x + y)
