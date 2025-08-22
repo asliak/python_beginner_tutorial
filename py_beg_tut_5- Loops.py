a8aa411bbacc0e0001778cece69d6d36fbd06b31
Aslı Nur Aksakal <92970740+asliak@users.noreply.github.com>
2025-08-22 23:19:52 +0300

    Add files via upload

---
+++ py_beg_tut_5- Loops.py
@@ -0,0 +1,27 @@
+x = 0
+
+while x < 10:
+    x += 1
+    print(x)
+
+for x in range(20):
+    print(x)
+
+for x in range(1, 21):
+    print(x)
+
+for x in range(20):
+    print("Hello World")
+
+x = 0
+while(x < 10):
+    if x == 5:
+        break
+    x += 1
+    print(x)
+
+while(x < 10):
+    x += 1
+    if x == 5:
+        continue
+    print(x)
